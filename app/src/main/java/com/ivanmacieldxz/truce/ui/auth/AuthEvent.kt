61a4a28f8d00a083db9531d73204a50f0586c5bd
Iván Maciel <ivanemanuelmaciel@gmail.com>
2026-07-30 00:50:23 -0300

    feat(auth): update state and viewmodel for multi-step signup and realtime validation

--- app/src/main/java/com/ivanmacieldxz/truce/ui/auth/AuthEvent.kt
+++ app/src/main/java/com/ivanmacieldxz/truce/ui/auth/AuthEvent.kt
@@ -1,9 +1,14 @@
 package com.ivanmacieldxz.truce.ui.auth
 
 sealed class AuthEvent {
+    data class FullNameChanged(val fullName: String) : AuthEvent()
     data class EmailChanged(val email: String) : AuthEvent()
     data class PasswordChanged(val password: String) : AuthEvent()
     data class UsernameChanged(val username: String) : AuthEvent()
     data object ToggleMode : AuthEvent()
+    data object NextStep : AuthEvent()
+    data object PreviousStep : AuthEvent()
+    data object TogglePasswordVisibility : AuthEvent()
+    data object ManualLoginCheck : AuthEvent()
     data object Submit : AuthEvent()
 }
